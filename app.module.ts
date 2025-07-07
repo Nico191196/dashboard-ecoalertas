ee1cba6644b581f99e9231d9dbb3f7a093b62ad4
Nico191196 <123272234+Nico191196@users.noreply.github.com>
2025-07-07 19:02:26 -0300

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -4,21 +4,39 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// Material Modules
+// Material
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 // Toastr
 import { ToastrModule } from 'ngx-toastr';
 
+// Translate
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
+
+// Components
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ReportService } from './core/services/report.service';
-import { SocketService } from './core/services/socket.service';
+import { ChartContainerComponent } from './dashboard/components/chart-container/chart-container.component';
+import { MapContainerComponent } from './dashboard/components/map-container/map-container.component';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    ChartContainerComponent,
+    MapContainerComponent
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
@@ -27,9 +45,19 @@ import { SocketService } from './core/services/socket.service';
     MatInputModule,
     MatSelectModule,
     MatButtonModule,
-    ToastrModule.forRoot({ timeOut: 3000, positionClass: 'toast-top-right' })
+    MatPaginatorModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ToastrModule.forRoot({ timeOut: 3000, positionClass: 'toast-top-right' }),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
-  providers: [ReportService, SocketService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
