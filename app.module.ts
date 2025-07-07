27b0a57e9fc6d2bb39a04e8ead7bee5c33dc9549
Nico191196 <123272234+Nico191196@users.noreply.github.com>
2025-07-07 18:54:28 -0300

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -2,6 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Material Modules
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+
+// Toastr
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -9,19 +18,18 @@ import { ReportService } from './core/services/report.service';
 import { SocketService } from './core/services/socket.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent
-  ],
+  declarations: [AppComponent, DashboardComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    ToastrModule.forRoot({ timeOut: 3000, positionClass: 'toast-top-right' })
   ],
-  providers: [
-    ReportService,
-    SocketService
-  ],
+  providers: [ReportService, SocketService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
